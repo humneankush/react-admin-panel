280fe696d490831a109b64fc2afad35ce3192083
humneankush <humneankush@gmail.com>
2021-07-20 18:02:08 +0530

    a normal linechar is created for the dummy data

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,17 @@
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredinfo/FeaturedInfo";
 import "./home.css";
-
+import { userData } from "../../dummyData";
 function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 }
